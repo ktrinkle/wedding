ca3956090e6142f5c7388045c4fd9f877d5e8a7f
Kevin Trinkle <70247961+ktrinkle@users.noreply.github.com>
2023-05-14 19:38:58 -0500

    Pipeline fix (#9)
    * Update pipeline
    
    * Update ef migration build
    
    * Add connection string env
    
    * Update connection string format
    
    * Update name order and prod URL
    
    * Update pipeline

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: "https://wedding.kevintrinkle.com",
-  app_url: "https://wedding.kevintrinkle.com"
+  api_url: "https://www.kevinandaustin.com",
+  app_url: "https://www.kevinandaustin.com"
 };
