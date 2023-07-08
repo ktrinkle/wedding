709cd4ac743da155cf8c5d0d298e2113dc38c874
Kevin Trinkle <trinkle@psa-history.org>
2023-07-08 08:05:00 -0500

    Add migration and update RSVP

---
+++ Migrations/20230708130423_DrinkTypeCd.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wedding.Migrations
+{
+    public partial class DrinkTypeCd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "drink_type_cd",
+                table: "wedding_group_name",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "drink_type_cd",
+                table: "wedding_group_name");
+        }
+    }
+}
